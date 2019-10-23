89cdd806257ba0a340ee0015ddc41e7fc590af25
Chiemeke Ifeanyi <chiemeke.ifeanyi@gmail.com>
2019-10-23 18:47:08 +0100

    fix bugs on account creation

---
+++ seeders/20191023170711-default-userTypes.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+      return queryInterface.bulkInsert('userTypes', [
+        {
+          name: 'learners',
+        },
+        {
+          name: 'educators',
+        },
+        {
+          name: 'admin',
+        }
+
+      ], {});
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
